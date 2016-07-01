7af9174060cc0574e4ee5533e5323aaefed7d92a
Charlotte Battersby <cbattersby@cbattersby-mlt.attlocal.net>
2016-07-01 11:29:01 -0700

    changing EventDetails to html file and added EventController.js

---
+++ DemoApp/app/js/controllers/EventController.js
@@ -0,0 +1,19 @@
+'use strict';
+
+eventsApp.controller('EventController',
+    function EventController($scope) {
+
+        $scope.event = {
+            name: 'Angular Boot Camp',
+            date: '1/1/2013',
+            time: '10:30am',
+            location: {
+                address: 'Google Headquarters',
+                city: 'Mountain View',
+                province: 'CA'
+            }
+        }
+
+    }
+    
+);
--- DemoApp/scripts/web-server.js
+++ DemoApp/scripts/web-server.js
@@ -4,6 +4,11 @@ var app = express();
 var rootPath = path.normalize(__dirname + '/../');
 
 app.use(express.static(rootPath + '/app'));
+app.set('view engine', 'jade');
+
+app.get('/EventDetails', function(req,res){
+    res.render('EventDetails');
+})
 
 app.listen(8000, function(){
     console.log('Listening on port 8000... ');
