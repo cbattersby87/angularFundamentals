6dc15515277202fe2fec2b22c83b7e8ea16c4af3
Charlotte Battersby <cbattersby@cbattersby-mlt.attlocal.net>
2016-07-01 11:47:53 -0700

    adding ng-repeat and adding more info to EventController

--- DemoApp/app/js/controllers/EventController.js
+++ DemoApp/app/js/controllers/EventController.js
@@ -11,7 +11,31 @@ eventsApp.controller('EventController',
                 address: 'Google Headquarters',
                 city: 'Mountain View',
                 province: 'CA'
-            }
+            }, 
+            imageUrl: '/img/angularjs-logo.png',
+            sessions: [
+                {
+                    name: 'Directives Masterclass',
+                    creatorName: 'Bob Smith',
+                    duration: '1 hr',
+                    level: 'Advanced',
+                    abstract: 'In this session you will learn the ins and outs of directives!'
+                },
+                {
+                    name: 'Scopes for fun and profit',
+                    creatorName: 'John Doe',
+                    duration: '30 min',
+                    level: 'Introductory',
+                    abstract: 'Learn what they do, how they do, and how to get them to work for you'
+                },
+                {
+                    name: 'Well Behaved Controllers',
+                    creatorName: 'Jane Doe',
+                    duration: '2 hr',
+                    level: 'Intermediate',
+                    abstract: 'Controllers are the beginning of everything in Angular. Learn how to craft controllers that will win the respect of your friends and family!'
+                }
+            ]
         }
 
     }
