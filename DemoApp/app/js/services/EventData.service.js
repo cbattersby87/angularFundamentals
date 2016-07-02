e2e099f5001c9fc65060cf98d3d737497390961e
Charlotte Battersby <cbattersby@cbattersby-mlt.attlocal.net>
2016-07-01 20:16:19 -0700

    adding new route, new event

--- DemoApp/app/js/services/EventData.service.js
+++ DemoApp/app/js/services/EventData.service.js
@@ -2,7 +2,7 @@ eventsApp.factory('eventData', function ($resource) {
     var resource = $resource('/data/event/:id', {id:'@id'});
     return {
         getEvent: function () {
-            return resource.get({id:1})
+            return resource.get({eventId})
         },
         save: function(event) {
             event.id = 999;
